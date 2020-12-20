0f464cc0a8d50afd8ecfa875ae8b7ba80c2eb1c4
maaya.sato <sato_maaya_nu@yahoo.co.jp>
2020-12-21 00:21:52 +0900

    Change import Timestamp

--- src/models/Task.java
+++ src/models/Task.java
@@ -1,6 +1,6 @@
 package models;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -54,8 +54,8 @@ public class Task {
         return created_at;
     }
 
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
+    public void setCreated_at(Timestamp currentTime) {
+        this.created_at = currentTime;
     }
 
     public Timestamp getUpdated_at() {
